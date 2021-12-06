00035576e276ab5f575cbf3aa9229dbf74418da0
Sami Akram <sameakram2@gmail.com>
2021-12-06 13:30:56 +0200

    spring boot starter

---
+++ pos_c/src/main/java/swer411/PosCApplication.java
@@ -0,0 +1,13 @@
+package swer411;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosCApplication.class, args);
+	}
+
+}
---
+++ pos_c/src/test/java/swer411/PosCApplicationTests.java
@@ -0,0 +1,13 @@
+package swer411;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
